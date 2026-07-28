5c1d657b480d2f7b8879f182b23fc55404667191
Ashish Nagar <ashishnagar033@gmail.com>
2026-07-28 23:26:06 +0530

    feat: protected routes updated

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,48 +1,29 @@
+"use client";
+import { useUserData } from "@/hooks/useUserData";
+import { redirect } from "next/navigation";
+import Appsidebar from "@/components/AppSidebar";
 
-'use client'
+const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isLoading, isError } = useUserData();
 
+  if (isLoading) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center gap-6">
+        <span className="font-extrabold text-2xl">iLynk</span>
+      </div>
+    );
+  }
 
-import { api } from "@/services/api"
-import { useQuery } from "@tanstack/react-query"
-import { AxiosError } from "axios"
-import { redirect } from "next/navigation"
-import { toast } from "sonner"
+  if (isError) {
+    redirect("/");
+  }
 
- const ProtectedLayout  = ({children}: {children: React.ReactNode}) => {
-
-     const {_data, isLoading, isError} = useQuery({
-        queryKey: ['currentUser'],
-        queryFn: async () => {
-           const res = await api.get('/user/me')
-           return res.data
-        },
-        retry: (count, err) => {
-            console.log('err ',count,"==" , err)
-            if(err instanceof AxiosError ) {
-              if(err.response?.status === 401){
-                toast.error('Unauthorized ! Please login')
-                return false
-              }
-            }
-            return true
-        }
-     })
- 
-
-     if(isLoading) {
-      return  <div className="h-screen w-screen flex justify-center items-center gap-6">
-      {/* <Spinner className="size-24" /> */}
-       <span className="font-extrabold text-2xl">iLynk</span>
+  return (
+    <div className="flex ">
+      <Appsidebar />
+      <main className="text-white flex-2">{children}</main>
     </div>
-     }
+  );
+};
 
-     if(isError) {
-        redirect('/')
-     }
-
-   return <div>
-    {children}
-   </div>
-}
-
-export default ProtectedLayout
+export default ProtectedLayout;

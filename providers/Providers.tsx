e0345cd3636c3e58f4305e1a2494ff07dc484c25
Ashish Nagar <ashishnagar033@gmail.com>
2026-07-31 23:33:26 +0530

    fix: react-query config update

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,23 +1,26 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";   
+import { useState } from "react";
 import { Toaster } from "sonner";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 2,
-        staleTime: 60 * 1000
-      }
-    }
-  }));
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
-        {children}
-       <Toaster />
+      {children}
+      <Toaster />
     </QueryClientProvider>
   );
 };
